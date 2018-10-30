2ac1a16d4b97da71ede27e50d08304d28053c477
Beata Markunas <beata.markunas@gmail.com>
2018-10-30 22:00:45 +0100

    Create counter.

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const button = document.querySelector("button");
+
+let number = 1;
+
+const addElement = function () {
+    const div = document.createElement("div");
+    div.textContent = number;
+    document.body.appendChild(div);
+    number++
+}
+
 button.addEventListener("click", addElement);
