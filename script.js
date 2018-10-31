758a43f3509782d6eaf251bf176d2278317ca7ce
Beata Markunas <beata.markunas@gmail.com>
2018-10-31 11:28:48 +0100

    Add conditional statement.

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ let number = 1;
 const addElement = function () {
     const div = document.createElement("div");
     div.textContent = number;
+    if (number % 5 === 0) {
+        div.classList.add("circle")
+    }
     document.body.appendChild(div);
-    number++
+    number++;
 }
 
 button.addEventListener("click", addElement);
